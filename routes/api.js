241469382bc5b3512acdf930abe1c347328589a6
Joe Kim <josephkim0224@gmail.com>
2017-10-29 08:56:22 -0400

    create get route for all profiles

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,30 @@
 var express = require('express')
 var router = express.Router()
 var Profile = require('../models/Profile')
+var Place = require('../models/Place')
 
 router.get('/:resource', function(req, res, next) {
   var resource = req.params.resource
+
+  if (resource == 'place') {
+    Place.find(null, function(err, results) {
+      if (err) {
+        res.json({
+          confirmation: 'fail',
+          message: err
+        })
+
+        return
+      }
+
+      res.json({
+        confirmation: 'success',
+        results: results
+      })
+
+      return
+    })
+  }
 
   if (resource == 'profile') {
     Profile.find(null, function(err, results) {
