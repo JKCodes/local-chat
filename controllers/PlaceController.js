999846740123569e095d84a4d2f7a033e671b549
Joe Kim <josephkim0224@gmail.com>
2017-10-30 16:24:26 -0400

    now creates geo data upon form submission

--- controllers/PlaceController.js
+++ controllers/PlaceController.js
@@ -1,5 +1,6 @@
 var Place = require('../models/Place')
 var Promise = require('bluebird')
+var superagent = require('superagent')
 
 module.exports = {
 
@@ -31,13 +32,45 @@ module.exports = {
 
   post: function(params) {
     return new Promise(function(resolve, reject) {
-      Place.create(params, function(err, place) {
-        if (err) {
+
+      // address format = 100+west+33rd+street,+new+york,ny
+      var address = params.address + ',' +params.city + ',' + params.state
+      address = address.replace(' ', '+')
+
+      var url = "https://maps.googleapis.com/maps/api/geocode/json"
+
+      var geoParams = {
+        address: address,
+        key: 'AIzaSyCAwPpxBVTQgFgmOQ0alCy1WitDxsNpdtQ'
+      }
+
+      superagent
+      .get(url)
+      .query(geoParams)
+      .set('Accept', 'text/json')
+      .end(function(err, response) {
+        if (err.status > 400) {
           reject(err)
+
           return
         }
 
-        resolve(place)
+        console.log(response)
+
+        var locationInfo = response.body.results[0]
+        var geometry = locationInfo.geometry
+        var latLng = geometry.location
+
+        params['geo'] = [latLng.lat, latLng.lng]
+
+        Place.create(params, function(err, place) {
+          if (err) {
+            reject(err)
+            return
+          }
+
+          resolve(place)
+        })
       })
     })
   },
