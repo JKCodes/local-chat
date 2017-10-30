bfdf3ab4b1aaafbbf5f4b8bc4ada88d98a3cc406
Joe Kim <josephkim0224@gmail.com>
2017-10-30 15:56:18 -0400

    get lat and lng from a hardcoded url

--- routes/geo.js
+++ routes/geo.js
@@ -1,15 +1,33 @@
 var express = require('express')
 var router = express.Router()
+var superagent = require('superagent')
 
 router.get('/', function(req, res, next) {
 
   var address = req.query.address
 
-  res.json({
-    confirmation: 'success',
-    address: address
-  })
+  var url = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCAwPpxBVTQgFgmOQ0alCy1WitDxsNpdtQ"
 
+  superagent
+  .get(url)
+  .query(null)
+  .set('Accept', 'text/json')
+  .end(function(err, response) {
+    if (err) {
+      res.json({
+        confirmation: 'fail',
+        message: err
+      })
+
+      return
+    }
+
+    var locationInfo = response.body.results[0]
+    var geometry = locationInfo.geometry
+    var latLng = geometry.location
+
+    res.send(latLng)
+  })
 })
 
 module.exports = router
