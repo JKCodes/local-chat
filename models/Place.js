0e7ed399a63869a42888ff19d5508b9d60005bb9
Joe Kim <josephkim0224@gmail.com>
2017-10-30 16:24:05 -0400

    added few required validation on Place schema

--- models/Place.js
+++ models/Place.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose')
 
 var PlaceSchema = new mongoose.Schema({
-  name: {type:String, default: ''},
+  name: {type:String, required:true, default: ''},
   description: {type:String, default: ''},
   type: {type:String, default: ''},
-  address: {type:String, default: ''},
-  city: {type:String, default: ''},
-  state: {type:String, default: ''},
-  zip: {type:String, default: ''},
+  address: {type:String, required:true, default: ''},
+  city: {type:String, required:true, default: ''},
+  state: {type:String, required:true, default: ''},
+  zip: {type:String, required:true, default: ''},
   geo: {
     type: [Number],
     index: '2d'
