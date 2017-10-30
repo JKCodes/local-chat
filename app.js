bb056cbc8d296a2b136aed43951c5558d0badb4b
Joe Kim <josephkim0224@gmail.com>
2017-10-30 16:23:43 -0400

    removed the unneeded geo route

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var sessions = require('client-sessions')
 var index = require('./routes/index')
 var api = require('./routes/api')
 var account = require('./routes/account')
-var geo = require('./routes/geo')
 
 var dbUrl = 'mongodb://localhost/local-chat'
 mongoose.connect(dbUrl, function(err, res) {
@@ -45,7 +44,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', index)
 app.use('/api', api)
 app.use('/account', account)
-app.use('/geo', geo)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
